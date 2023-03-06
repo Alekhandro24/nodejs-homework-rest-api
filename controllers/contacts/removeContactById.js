3a448f9bdc33e7c33d92dd9393bf946674693392
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-03-06 23:34:46 +0200

    Create removeContactById.js

---
+++ controllers/contacts/removeContactById.js
@@ -0,0 +1,20 @@
+const { NotFound } = require("http-errors");
+const { Contact } = require("../../models");
+
+const removeContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
+  if (!result) {
+    throw new NotFound(`Contact with id=${id} not found`);
+  }
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    message: "contact deleted",
+    data: {
+      result,
+    },
+  });
+};
+
+module.exports = removeContact;
