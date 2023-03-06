8c8f853688acbe4fa793f87bbe34edeaa0ac8671
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-03-06 23:33:01 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,22 @@
 const express = require("express");
 
-const { validation, ctrlWrapper } = require("../../middlewares");
+const { auth, validation, ctrlWrapper } = require("../../middlewares");
 const { joiSchema, favoriteSchema } = require("../../models/contact");
 const { contacts: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.listContacts));
+router.get("/", auth, ctrlWrapper(ctrl.listContacts));
 
 router.get("/:id", ctrlWrapper(ctrl.getContactById));
 
-router.post("/", validation(joiSchema), ctrlWrapper(ctrl.addContact));
+router.post("/", auth, validation(joiSchema), ctrlWrapper(ctrl.addContact));
 
 router.put("/:id", validation(joiSchema), ctrlWrapper(ctrl.updateContact));
 
 router.patch(
   "/:id/favorite",
+  auth,
   validation(favoriteSchema),
   ctrlWrapper(ctrl.updateFavorite)
 );
