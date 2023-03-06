368161f02b17dd92c9d5ab9ca6f500e577b356a8
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-03-06 23:34:14 +0200

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,7 +1,18 @@
 const { Contact } = require("../../models");
 
 const listContacts = async (req, res) => {
-  const contacts = await Contact.find({});
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+
+  let filter;
+
+  favorite ? (filter = { owner, favorite }) : (filter = { owner });
+
+  const contacts = await Contact.find(filter, "-createAt -updateAt", {
+    skip,
+    limit: Number(limit),
+  });
   res.json({
     status: "success",
     code: 200,
