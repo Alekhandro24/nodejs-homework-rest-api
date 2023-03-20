23e9b4ef2d7f631e5f27951682d1628178fe495b
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-03-20 21:17:10 +0200

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,11 @@
 const bcrypt = require("bcryptjs");
 const { Conflict } = require("http-errors");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+
+const { sendEmail } = require("../../helpers");
 const { User } = require("../../models");
+const { BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password, subscription } = req.body;
@@ -11,6 +15,7 @@ const register = async (req, res) => {
     throw new Conflict(`User with ${email} already exist`);
   }
 
+  const verificationToken = nanoid();
   const avatarURL = gravatar.url(email);
 
   const hashPassword = await bcrypt.hash(password, 10);
@@ -19,7 +24,17 @@ const register = async (req, res) => {
     password: hashPassword,
     subscription,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Confirm your email",
+    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Confirm your email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
   res.status(201).json({
     status: "success",
     code: 201,
